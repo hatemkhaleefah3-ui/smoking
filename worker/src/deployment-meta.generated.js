5d5f791969f984aa106f3e9eb7a39c0130ba78a7
hatemkhaleefah3-ui <hatemkhaleefah3@gmail.com>
2026-07-29 07:19:57 +0300

    Add deployment metadata placeholder

---
+++ worker/src/deployment-meta.generated.js
@@ -0,0 +1,3 @@
+export const DEPLOYMENT_COMMIT_SHA = 'unknown';
+export const FIX_COMMIT_SHA = '1a1ef610e1326e2f110f1956655066ff383c6a9e';
+export const DEPLOYMENT_METADATA_GENERATED_AT = 'not-generated';
